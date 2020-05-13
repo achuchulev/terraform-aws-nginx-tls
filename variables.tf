e77a8b7e0ba807ecc4122911e0f805250fc8d2c6
Atanas Chuchulev <atanas.v4@gmail.com>
2020-05-13 17:35:57 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+# Provider VARs
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+# Instance VARs
+variable "vpc_id" {}
+variable "subnet_id" {}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami" {
+  description = "Ubuntu 18.04 LTS AMI in AWS us-east-1 region"
+  default     = "ami-085925f297f89fce1"
+}
